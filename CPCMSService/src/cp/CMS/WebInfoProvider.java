f6dea1bc10ac1220e0946196fc9d99c7af4971a5
xieqj <xqj198404@gmail.com>
2014-08-28 17:21:12 +0800

    201402281721

--- CPCMSService/src/cp/CMS/WebInfoProvider.java
+++ CPCMSService/src/cp/CMS/WebInfoProvider.java
@@ -1,5 +1,30 @@
 package cp.CMS;
 
-public class WebInfoProvider {
+import java.util.List;
 
+import cp.Base.GridPage;
+import cp.Base.OperationResult;
+import cp.Base.WebInfoModel;
+
+public class WebInfoProvider {
+	//获取模板的分页数据
+		 public GridPage<List<WebInfoModel>> GetPageList(int currentPage, int pageSize, String searchKey)
+	     {         
+	         return null;
+	     }
+		 //按照模板id获取模板内容
+	     public  OperationResult<WebInfoModel> GetWebInfoById(int infoId)
+	     {
+	    	 return null;
+	     }
+	    //模板添加与更新
+	     public  OperationResult<Boolean> WebInfoModelUpdate()
+	     {
+	    	 return null;
+	     }
+	     //删除模板
+	     public  OperationResult<Boolean> DeleteWebInfoModels(int[] infoIds, int operationBy)
+	     {
+	    	 return null;
+	     }
 }
